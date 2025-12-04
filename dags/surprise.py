b2f87b2ffece9fe0a3fda16477a22294a210dfb6
Marc Lamberti <marclamberti.ml@gmail.com>
2025-12-04 13:09:35 +0400

    update

--- dags/surprise.py
+++ dags/surprise.py
@@ -1,21 +1,20 @@
-from airflow.decorators import dag, task
+from airflow.sdk import dag, task
 
 @dag(
-    dag_id='surprise',
     description='Run it and get your gift...',
     schedule=None,
     tags=['surprise', 'gift'],
 )
 def surprise():
     
-    @task()
+    @task
     def look_at_my_logs():
         import os
         import hashlib
 
         node_selector = os.getenv('ASTRONOMER_NODE_SELECTOR', 'you-must-run-this-dag-on-astro')
         if node_selector == 'you-must-run-this-dag-on-astro':
-            print(f"Get your free certification https://academy.astronomer.io/astronomer-certified-apache-airflow-core-exam?pc={node_selector}")
+            print(f"Get your free certification https://academy.astronomer.io/certification-exam-apache-airflow-3-fundamentals?pc={node_selector}")
             return
 
         hash_object = hashlib.sha256(node_selector.encode())
@@ -28,7 +27,7 @@ def surprise():
             index = int(hex_hash[pos], 16) % len(charset)
             result += charset[index]
 
-        print(f"Get your free certification https://academy.astronomer.io/astronomer-certified-apache-airflow-core-exam?pc={result}")
+        print(f"Get your free certification https://academy.astronomer.io/certification-exam-apache-airflow-3-fundamentals?pc={result}")
 
     look_at_my_logs()
 
